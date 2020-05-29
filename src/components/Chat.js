50f8d9f7620b8c3f96cc0ae97884c86efe2261fc
demkantor <demkantor@gmail.com>
2020-05-29 18:19:40 -0500

    initial setup and commit

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <h1>Chat with me!</h1>
+    )
+};
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Join = () => {
+    return (
+        <h1>Join this chat!</h1>
+    )
+};
+
+export default Join;
