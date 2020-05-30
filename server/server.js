380f5b444b42ad66cefd076f7aa5f289ba3a05ad
demkantor <demkantor@gmail.com>
2020-05-30 00:14:43 -0500

    ability to send messages to server added

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ const io = socketio(server);
 const router = require('./router');
 app.use(router);
 
+
 io.on('connection', (socket) => {
+    // handles when a new user joins a room
     socket.on('join', ({ name, room }, callback)=> {
         const { error, user } = addUser({ id: socket.id, name, room });
 
@@ -21,11 +23,20 @@ io.on('connection', (socket) => {
 
         socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room ${user.room}!` });
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!!` });
-
         socket.join(user.room);
 
         callback();
     });
+
+    // handles when a user sends a message
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
 
     socket.on('disconnect', () => {
         console.log('User has left!!');
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -8,8 +8,12 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    // declares our url
     const ENDPOINT = 'localhost:5000';
 
+    // handles when a user joins or leaves a room
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
@@ -29,8 +33,32 @@ const Chat = ({ location }) => {
         
     }, [ENDPOINT, location.search]);
 
+    // add new messags
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    // handles sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    };
+
+    console.log(message, messages)
+
     return (
-        <h1>Chat with me!</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                    value={message} 
+                    onChange={(event) => setMessage(event.target.value)} 
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} />
+            </div>
+        </div>
     )
 };
 
